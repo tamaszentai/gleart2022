3ad801bc25e556207751ef5a87b6cb3b68c95e97
Tamas Zentai <zentaitamas@gmail.com>
2022-03-12 09:06:32 +0000

    hello

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Portfolio</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/pricelist">Pricelist</router-link> |
+    <router-link to="/contact">Contact</router-link>
   </div>
   <router-view />
 </template>
 
 <style lang="less">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Cuprum", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -16,11 +18,12 @@
 }
 
 #nav {
-  padding: 30px;
+  padding: 3rem;
 
   a {
     font-weight: bold;
     color: #2c3e50;
+    padding: 1rem;
 
     &.router-link-exact-active {
       color: #42b983;
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
+import Portfolio from "../views/Portfolio.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "Home",
-    component: Home,
+    component: Portfolio,
   },
   {
     path: "/about",
@@ -15,6 +15,24 @@ const routes: Array<RouteRecordRaw> = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/pricelist",
+    name: "Pricelist",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Pricelist.vue"),
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Contact.vue"),
   },
 ];
 
